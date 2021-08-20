244df96da9024968bc114fd3d2546b27bb251a06
flipflop133 <francois.bechet@tutanota.com>
2021-08-20 11:59:08 +0200

    update dots

--- scripts/.config/scripts/python/spotify.py
+++ scripts/.config/scripts/python/spotify.py
@@ -1,8 +1,6 @@
-# imports
 import os
-from subprocess import check_output, run, PIPE
+from subprocess import run, PIPE
 import time
-from time import sleep
 import json
 import sys
 try:
@@ -25,130 +23,126 @@ class Dbus:
     intf_player = obj_player + ".Player"
 
 
-def muteSpotify(mute):
-    # run the command
-    appList = run(["pacmd", "list-sink-inputs"], stdout=PIPE)
+class Spotify:
+    song = ""
+    artist = ""
+    album = ""
+    ad = False
 
-    # parse the output
-    parsed = {}
-    lines = appList.stdout.decode('utf-8').replace("\t",
-                                                   "").replace(" ",
-                                                               "").split("\n")
-    current_index = 0
-    cross_properties = False
+    def __init__(self):
+        while True:
+            # retrieve Spotify metadata
+            Spotify.getSongData(self)
+            # display Spotify metadata
+            Spotify.output(self)
+            # check and block ads
+            Spotify.blockAds(self)
+            time.sleep(3)
 
-    for line in lines:
-        if not cross_properties:
-            splited = line.split(":")
-            cross_properties = splited[0] == "properties"
+    def muteSpotify(self, mute):
+        """ Mute Spotify sound
+        """
+        # retrieve sink inputs
+        appList = run(["pacmd", "list-sink-inputs"], stdout=PIPE)
 
-            if splited[0] == "index":
-                current_index = int(splited[1])
+        # parse the output
+        parsed = {}
+        lines = appList.stdout.decode('utf-8').replace("\t", "").replace(
+            " ", "").split("\n")
+        current_index = 0
+        cross_properties = False
 
-        else:
-            splited = line.split("=")
-            if splited[0] == "application.name":
-                parsed[splited[1]] = current_index
-                current_index = 0
-                cross_properties = False
+        for line in lines:
+            if not cross_properties:
+                splited = line.split(":")
+                cross_properties = splited[0] == "properties"
 
-    # mute
-    index = parsed["\"Spotify\""]
-    run(["pacmd", "set-sink-input-mute", str(index), mute])
+                if splited[0] == "index":
+                    current_index = int(splited[1])
 
-
-def blockAds(song):
-    """Mute sound during ads
-    """
-    # mute sound if spotify is playing and AD
-    if 'Advertisement' in song:
-        muteSpotify("1")
-        ad = True
-        time.sleep(5)
-    if 'Advertisement' not in song:
-        muteSpotify("0")
-        ad = False
-    return ad
-
-
-def getSong():
-    """Retrieve Spotify current playing song using dbus
-    """
-    session_bus = dbus.SessionBus()
-    spotify_bus = session_bus.get_object('org.mpris.MediaPlayer2.spotify',
-                                         '/org/mpris/MediaPlayer2')
-
-    spotify_properties = dbus.Interface(spotify_bus,
-                                        'org.freedesktop.DBus.Properties')
-    metadata = spotify_properties.Get('org.mpris.MediaPlayer2.Player',
-                                      'Metadata')
-    artist = metadata['xesam:artist'][0] if metadata['xesam:artist'] else ''
-    song = metadata['xesam:title'] if metadata['xesam:title'] else ''
-    album = metadata['xesam:album'] if metadata['xesam:album'] else ''
-
-    # check for ads
-    blockAds(song)
-
-    # determine icon
-    status = get_playBackStatus()
-    if status == "Paused":
-        icon = "Paused"
-    else:
-        icon = "Playing"
-
-    # display song name
-    if (song != '' or artist != ''
-            or album != '') and 'Advertisement' not in song:
-        if (artist != '' and song != ''):
-            output = {'text': song[:15] + " - " + artist[:15], 'alt': icon}
-        elif (artist != ''):
-            output = {'text': artist[:20], 'alt': icon}
-        elif (song != ''):
-            output = {'text': song[:20], 'alt': icon}
-        else:
-            output = {'text': album[:20], 'alt': icon}
-    sys.stdout.write(json.dumps(output) + '\n')
-    sys.stdout.flush()
-    return ad
-
-
-def get_playBackStatus():
-    """Use Dbus to find the current playback status
-    """
-    player = Dbus.obj_player + "." + "spotify"
-    player = dbus.SessionBus().get_object(player, Dbus.path_player)
-    properties = dbus.Interface(player, Dbus.intf_props)
-    return properties.Get(Dbus.intf_player, "PlaybackStatus")
-
-
-def main():
-    try:
-        data = None
-        try:
-            with open("/tmp/spotify.json", "r") as read_file:
-                data = json.load(read_file)
-        except IOError:
-            dict = {"ad": False, "isSong": False}
-            with open("/tmp/spotify.json", "w+") as read_file:
-                json.dump(dict, read_file)
-        if data is not None:
-            ad = bool(data['ad'])
-            isSong = bool(data['isSong'])
-            values = getSong()
-            if values == "no_process":
-                print("")
             else:
-                isSong = values[0]
-                ad = values[1]
-                dict = {"ad": ad, "isSong": isSong}
-                with open("/tmp/spotify.json", "w") as read_file:
-                    json.dump(dict, read_file)
+                splited = line.split("=")
+                if splited[0] == "application.name":
+                    parsed[splited[1]] = current_index
+                    current_index = 0
+                    cross_properties = False
 
-    except Exception as e:
-        if isinstance(e, dbus.exceptions.DBusException):
-            print('')
+        # mute Spotify
+        index = parsed["\"Spotify\""]
+        run(["pacmd", "set-sink-input-mute", str(index), mute])
+
+    def blockAds(self):
+        """Mute sound during ads
+        """
+        if 'Advertisement' in Spotify.song and Spotify.ad is False:
+            Spotify.muteSpotify(self, "1")
+            Spotify.ad = True
+        if 'Advertisement' not in Spotify.song and Spotify.ad is True:
+            Spotify.muteSpotify(self, "0")
+            Spotify.ad = False
+
+    def getSongData(self):
+        """Retrieve Spotify current playing song data using dbus
+        """
+        session_bus = dbus.SessionBus()
+        spotify_bus = session_bus.get_object('org.mpris.MediaPlayer2.spotify',
+                                             '/org/mpris/MediaPlayer2')
+
+        spotify_properties = dbus.Interface(spotify_bus,
+                                            'org.freedesktop.DBus.Properties')
+        metadata = spotify_properties.Get('org.mpris.MediaPlayer2.Player',
+                                          'Metadata')
+        artist = metadata['xesam:artist'][0] if metadata['xesam:artist'] else ''
+        Spotify.artist = artist
+        song = metadata['xesam:title'] if metadata['xesam:title'] else ''
+        Spotify.song = song
+        album = metadata['xesam:album'] if metadata['xesam:album'] else ''
+        Spotify.album = album
+
+    def output(self):
+        """ Print song data in Json format.
+        """
+        output = ""
+        # determine icon
+        status = Spotify.getPlayBackStatus(self)
+        if status == "Paused":
+            icon = "Paused"
         else:
-            print('')
+            icon = "Playing"
+
+        # display song name
+        if (Spotify.song != '' or Spotify.artist != '' or
+                Spotify.album != '') and 'Advertisement' not in Spotify.song:
+            if (Spotify.artist != '' and Spotify.song != ''):
+                output = {
+                    'text': Spotify.song[:20] + " - " + Spotify.artist[:20],
+                    'alt': icon,
+                    'tooltip': Spotify.album
+                }
+            elif (Spotify.artist != ''):
+                output = {
+                    'text': Spotify.artist[:40],
+                    'alt': icon,
+                    'tooltip': Spotify.album
+                }
+            elif (Spotify.song != ''):
+                output = {
+                    'text': Spotify.song[:40],
+                    'alt': icon,
+                    'tooltip': Spotify.album
+                }
+            else:
+                output = {'text': Spotify.album[:40], 'alt': icon}
+        sys.stdout.write(json.dumps(output) + '\n')
+        sys.stdout.flush()
+
+    def getPlayBackStatus(self):
+        """Use Dbus to find the current playback status
+        """
+        player = Dbus.obj_player + "." + "spotify"
+        player = dbus.SessionBus().get_object(player, Dbus.path_player)
+        properties = dbus.Interface(player, Dbus.intf_props)
+        return properties.Get(Dbus.intf_player, "PlaybackStatus")
 
 
-main()
+Spotify()
